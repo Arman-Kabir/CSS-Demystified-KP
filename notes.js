d9394e429dd310c4a78511ff36224dce70f8e6dd
Arman-Kabir <armankabir8@gmail.com>
2023-03-29 20:19:58 +0600

    relationships 8 done

---
+++ notes.js
@@ -0,0 +1,12 @@
+/**#8
+ * **The relationships between:
+ * An element and the viewport
+ * An element and it's parent
+ * Sibling elements
+ * 
+ * **If the parent has a width and we declare a width to the child, it might overflow out the side or it might be a percentage of the parent's width.
+ * 
+ * *These relationships are controlled by a given elements formatting context.
+ * 
+ * **Layouts are all about Relationships between elements.
+ */
